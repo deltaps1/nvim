27b7d5c2deaa5ec181db5b57cdb664a209f52af3
Karsten <deltapsi@icloud.com>
2024-05-13 01:44:46 +0200

    Added a 'go back' shortcut

--- lua/deltapsi/remap.lua
+++ lua/deltapsi/remap.lua
@@ -35,3 +35,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- chmod +x ftw!
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Go back to previous file
+vim.keymap.set("n", "gb", "<cmd>e#<CR>")
