1dd78873b6be79e9d55b41f6dc5fc80223384254
Karsten <deltapsi@icloud.com>
2024-05-12 18:58:19 +0200

    I moved conf in init.lua to profile init

--- lua/deltapsi/init.lua
+++ lua/deltapsi/init.lua
@@ -1,2 +1,23 @@
 require("deltapsi.remap")
 require("deltapsi.lazy")
+
+vim.wo.relativenumber = true
+
+-- Sæt standard indrykning til 4 mellemrum
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+
+-- Funktion til at sætte indrykning for specifikke filtyper
+local function set_indent(width)
+    vim.o.shiftwidth = width
+    vim.o.tabstop = width
+end
+
+-- Brug autocmd til at sætte indrykning til 2 mellemrum for JSX-filer
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"javascriptreact", "typescriptreact"},
+    callback = function()
+        set_indent(2)
+    end,
+})
