e8541b8961060319830ae114098a3ef8eb434a4b
Karsten <deltapsi@icloud.com>
2024-06-07 23:19:06 +0200

    Remapped -> colission with Fugitive

--- lua/deltapsi/remap.lua
+++ lua/deltapsi/remap.lua
@@ -37,7 +37,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Go back to previous file
-vim.keymap.set("n", "gb", "<cmd>e#<CR>")
+vim.keymap.set("n", "gn", "<cmd>e#<CR>")
 
 -- Nice resizes?
 vim.keymap.set('n', '<C-h>', ':vertical resize -2<CR>')
