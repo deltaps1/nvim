151abe35d62d8525d46631c0e25fd44f306b925f
Karsten Skjellerup <deltapsi@icloud.com>
2025-01-08 11:10:11 +0100

    Fixed missing popup menu

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,6 +18,15 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
   preselect = 'item',
   completion = {
     completeopt = 'menu,menuone,noinsert'
